19e2d71a87317cad10a855bd979bbc540c9c3e36
arpanaray77 <arpanaray75897@gmail.com>
2021-05-05 17:41:14 +0530

    Modified Edit Distance

--- EditDistance.java
+++ EditDistance.java
@@ -5,15 +5,19 @@ class Solution {
  
         int mat[][] = new int[m + 1][n + 1];
  
+        //base conditions
         for (int i = 0; i <= m; i++) mat[i][0] = i;
-        for (int i = 0; i <= n; i++) mat[0][i] = i;
+        for (int j = 0; j <= n; j++) mat[0][j] = j;
  
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
-                int cost = 1;
-                if (word1.charAt(i - 1) == word2.charAt(j - 1)) cost = 0;
- 
-                mat[i][j] = min(mat[i - 1][j] + 1, mat[i][j - 1] + 1, mat[i - 1][j - 1] + cost);
+                
+                if (word1.charAt(i - 1) == word2.charAt(j - 1))
+   //as last character is same we have to manipulate only (m-1) accpding to (n-1)characters
+                       mat[i][j]=mat[i-1][j-1];
+                else
+    //find minimum operations required after performing deletion,insertion and substituion for last charcter of word1 to make it equal to word2
+                   mat[i][j] =1+ min(mat[i - 1][j], mat[i][j - 1], mat[i - 1][j - 1]);
             }
         }
         return mat[m][n];
